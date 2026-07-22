96039b38a576ed29cee540fcc9325d0580ebb41e
Ravi Kant Jha <ravirbikant@gmail.com>
2026-07-22 13:45:01 +0530

    Reverse array without inbuilt methods

---
+++ reverseArrayWithoutInbuiltMethods.js
@@ -0,0 +1,14 @@
+var reverseString = function(s) {
+    let temp;
+    for(let i = 0; i<Math.floor(s.length/2);i++){
+        temp = s[i];
+        s[i] = s[s.length - 1 - i];
+        s[s.length - 1 - i] = temp;
+    }
+    
+    return s;
+};
+
+let s = ["h","e","l","l","o"];
+
+console.log(reverseString(s));
